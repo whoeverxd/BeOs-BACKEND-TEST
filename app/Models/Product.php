7ad1f3c124f60f2a8a2f6c483980c9177a3600d4
whoeverxd <joshuamoises1995@gmail.com>
2026-03-27 21:42:08 -0400

    version inicial del modelo de Producto

---
+++ app/Models/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'currency_id',
+        'tax_cost',
+        'manufacturing_cost',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'currency_id' => 'integer',
+            'tax_cost' => 'decimal:2',
+            'manufacturing_cost' => 'decimal:2',
+        ];
+    }
+}
